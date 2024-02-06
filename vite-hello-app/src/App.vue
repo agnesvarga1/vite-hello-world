f894d14bd4951b7a629a3feb1f6824d112a7b5ed
agnesvarga1 <agikolyok@gmail.com>
2024-02-06 16:02:38 +0100

    1st commit with component

---
+++ vite-hello-app/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppTitle from "../src/components/appTitle.vue";
+export default {
+  name: "App",
+  components: {
+    AppTitle,
+  },
+};
+</script>
+
+<template>
+  <h1>Haliho</h1>
+  <AppTitle />
+</template>
+
+<style>
+body {
+  background-color: #121212;
+}
+h1 {
+  color: #8286f4;
+}
+</style>
